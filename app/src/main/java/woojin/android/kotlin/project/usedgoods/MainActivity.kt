c297b2a37f0fa0bf59bdd6e9bf2039b6cf3311d0
kwjjjjj <kwjjjjj@abrain.co.kr>
2021-09-07 11:04:35 +0900

    [added] 프로젝트 생성 및 바텀네비게이션 뷰 설정

---
+++ app/src/main/java/woojin/android/kotlin/project/usedgoods/MainActivity.kt
@@ -0,0 +1,11 @@
+package woojin.android.kotlin.project.usedgoods
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
